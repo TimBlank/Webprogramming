9d0140f0cb405f0309cf911412c6b2f68b9a4d44
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-05 13:18:06 +0200

    Navigation und Suchleiste in eigenen Dateien

---
+++ php/navigation.php
@@ -0,0 +1,39 @@
+<nav>
+    <ul>
+        <li><a href="../../Index.php" title="Hauptseite">
+                Start
+            </a></li>
+        <li><a href="../../Karte.php" title="Haarentor">
+                Haarentor
+            </a></li>
+        <li><a href="../../Karte.php" title="Wechloy">
+                Wechloy
+            </a></li>
+        <li>Weitere
+            <ul>
+                <li><a href="../../Karte.php" title="Johann-Justus-Weg">
+                        Johann-Justus-Weg
+                    </a></li>
+                <li><a href="../../Karte.php" title="Botanischer Garten">
+                        Botanischer Garten
+                    </a></li>
+                <li><a href="../../Karte.php" title="Offis">
+                        Offis
+                    </a></li>
+            </ul>
+        </li>
+        <li>
+            Loging/Registrierung
+            <ul>
+                <li>
+                    <form>
+                        <input type="text" id="username" name="un" value="" placeholder="Benutzername" autocomplete="off" />
+                        <input type="text" id="password" name="pw" value="" placeholder="Passwort" autocomplete="off" />
+                        <input type="submit">
+                    </form>
+                    <a href="../../Regestrierung.php" title="Registrieren">Registrieren </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</nav>
