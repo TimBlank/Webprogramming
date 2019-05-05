bc6e95ca68e102530008a95ae51ff035fbea036c
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-06 01:08:44 +0200

    Erste Version einer Navigationsleiste hinzugefügt

--- php/navigation.php
+++ php/navigation.php
@@ -1,39 +1,43 @@
-<nav>
-    <ul>
-        <li><a href="../../Index.php" title="Hauptseite">
-                Start
-            </a></li>
-        <li><a href="../../Karte.php" title="Haarentor">
-                Haarentor
-            </a></li>
-        <li><a href="../../Karte.php" title="Wechloy">
-                Wechloy
-            </a></li>
-        <li>Weitere
-            <ul>
-                <li><a href="../../Karte.php" title="Johann-Justus-Weg">
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr- auto">
+            <li class="nav-item active"><a href="../../Index.php" title="Hauptseite">
+                    Start
+                </a></li>
+            <li class="nav-item"><a href="../../Karte.php" title="Haarentor">
+                    Haarentor
+                </a></li>
+            <li class="nav-item"><a href="../../Karte.php" title="Wechloy">
+                    Wechloy
+                </a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Weitere</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="../../Karte.php" title="Johann-Justus-Weg">
                         Johann-Justus-Weg
-                    </a></li>
-                <li><a href="../../Karte.php" title="Botanischer Garten">
+                    </a>
+                    <a class="dropdown-item" href="../../Karte.php" title="Botanischer Garten">
                         Botanischer Garten
-                    </a></li>
-                <li><a href="../../Karte.php" title="Offis">
+                    </a>
+                    <a class="dropdown-item" href="../../Karte.php" title="Offis">
                         Offis
-                    </a></li>
-            </ul>
-        </li>
-        <li>
-            Loging/Registrierung
-            <ul>
-                <li>
-                    <form>
-                        <input type="text" id="username" name="un" value="" placeholder="Benutzername" autocomplete="off" />
-                        <input type="text" id="password" name="pw" value="" placeholder="Passwort" autocomplete="off" />
-                        <input type="submit">
-                    </form>
-                    <a href="../../Regestrierung.php" title="Registrieren">Registrieren </a>
-                </li>
-            </ul>
-        </li>
-    </ul>
+                    </a>
+                </div>
+
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Login/Registrierung</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item">
+                        <form>
+                            <input type="text" id="username" name="un" value="" placeholder="Benutzername" autocomplete="off" />
+                            <input type="text" id="password" name="pw" value="" placeholder="Passwort" autocomplete="off" />
+                            <input type="submit">
+                        </form>
+                        <a href="../../Regestrierung.php" title="Registrieren">Registrieren </a>
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </div>
 </nav>
