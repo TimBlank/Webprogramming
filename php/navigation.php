d68fe649ca823df9acdc8aa5d90cda8670f8cda5
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-07 16:17:15 +0200

    Login weiter rechts

--- php/navigation.php
+++ php/navigation.php
@@ -1,48 +1,54 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mr- auto ">
-            <li class="nav-item active"><a class="nav-link" href="../../Index.php" title="Hauptseite">
-                    Start
-                    <span class="sr-only">(current)</span>
-                </a></li>
-            <li class="nav-item"><a class="nav-link" href="../../Karte.php" title="Haarentor">
-                    Haarentor
-                </a></li>
-            <li class="nav-item"><a class="nav-link" href="../../Karte.php" title="Wechloy">
-                    Wechloy
-                </a></li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="Weitere Standorte">Weitere</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="../../Karte.php" title="Johann-Justus-Weg">
-                        Johann-Justus-Weg
-                    </a>
-                    <a class="dropdown-item" href="../../Karte.php" title="Botanischer Garten">
-                        Botanischer Garten
-                    </a>
-                    <a class="dropdown-item" href="../../Karte.php" title="Offis">
-                        Offis
-                    </a>
-                </div>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr- auto ">
+                <li class="nav-item active"><a class="nav-link" href="../../Index.php" title="Hauptseite">
+                        Start
+                        <span class="sr-only">(current)</span>
+                    </a></li>
+                <li class="nav-item"><a class="nav-link" href="../../Karte.php" title="Haarentor">
+                        Haarentor
+                    </a></li>
+                <li class="nav-item"><a class="nav-link" href="../../Karte.php" title="Wechloy">
+                        Wechloy
+                    </a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="Weitere Standorte">Weitere</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="../../Karte.php" title="Johann-Justus-Weg">
+                            Johann-Justus-Weg
+                        </a>
+                        <a class="dropdown-item" href="../../Karte.php" title="Botanischer Garten">
+                            Botanischer Garten
+                        </a>
+                        <a class="dropdown-item" href="../../Karte.php" title="Offis">
+                            Offis
+                        </a>
+                    </div>
 
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Login/Registrierung</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item">
-                        <form class="form-inline">
-                            <input type="text" id="username" name="un" value="" placeholder="Benutzername" autocomplete="off" />
-                            <input type="text" id="password" name="pw" value="" placeholder="Passwort" autocomplete="off" />
-                            <input type="submit">
-                        </form>
-                        <a href="../../Regestrierung.php" title="Registrieren">Registrieren </a>
-                    </a>
-                </div>
-            </li>
-        </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="collapse navbar-collapse navbar-right" id="navbarNav">
+            <ul class="navbar-nav navbar-right">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Login/Registrierung</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item">
+                            <form class="form-inline">
+                                <input type="text" id="username" name="un" value="" placeholder="Benutzername" autocomplete="off" />
+                                <input type="text" id="password" name="pw" value="" placeholder="Passwort" autocomplete="off" />
+                                <input type="submit">
+                            </form>
+                            <a href="../../Regestrierung.php" title="Registrieren">Registrieren </a>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
