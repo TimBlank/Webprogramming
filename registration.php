6034946163b5f4a1d0b54f581e63ed87504efe1e
CheaseForFree <CheaseForFree@users.noreply.github.com>
2019-05-08 13:17:50 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" href="css/struktur.css">
+    <link rel="stylesheet" type="text/css" href="css/structure.css">
 
     <title>Fahrrad Stellpätze</title>
     <link href="Bilder/IconTransparent.png" rel="icon">
