2b607f3872f5b6754205b05772cd78bfe1a10b59
Tim Behrens <timy.behrens@ewetel.net>
2019-05-09 10:02:51 +0200

    versuch mit bootstrap

--- Index.php
+++ Index.php
@@ -18,50 +18,69 @@
     <?php include "php/search.php"; ?>
 
     <section>
-        <article >
+         <article >
 
             <h1>Über diese Seite</h1>
             Auf dieser Seite kannst du die Fahrradstellplätze in der Nähe der Carl von Ossietzky Universität in Oldenburg finden und regestrierte Benutzer können fehlende Plätze hinzufügen.
         </article>
         <ul>
             <li>
-                <div style="border: black 1px solid";>
-                    <h1><a href="Beitraege/W6West/W6West.php" title="W6 West" style="border: black 1px solid"; >
-                            W6 West
+                <div class="card" style="width: 75rem;" style="border: black 1px solid">
+                    <div class="card-body">
+                        <h1><a class="card-title" title="W6 West"  >
+                             W6 West
                         </a></h1>
-                    <img class="lins" src="Beitraege/W6West/ink.png" alt="Bild des Stellplatzes" height="300" style="border: black 1px solid"; >
-                    <p style="border: black 1px solid";>Wechloy</p>
-                    <p style="border: black 1px solid";>Überdacht</p>
-                    <p style="border: black 1px solid";>Öffentlich</p>
-                    <img src="Beitraege/W6West/BildDerKarte.png" alt="Position des Stellplatzes" style="border: black 1px solid";  >
+                        <div calss="row" style="border: black 1px solid">
+                            <div class="card">
+                                <div class="col-sm-4">
+                                    <img class="card-img-top" src="Beitraege/W6West/ink.png" alt="Bild des Stellplatzes"  style="border: black 1px solid" height="300" width="300">
+                                </div>
+                            </div>
+                            <div class ="card">
+                                    <div class ="col-sm-4" style="border: black 1px solid">
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Wechloy</li>
+                                        <li class="list-group-item">Überdacht</li>
+                                        <li class="list-group-item">Öffentlich</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class= "col-sm-4">
+                                    <img class="card-img-top" src="Beitraege/W6West/BildDerKarte.png" alt="Position des Stellplatzes" style="border: black 1px solid" height="300" width="300" >
+                                </div>
+                             </div>
+                              <a href="Beitraege/W6West/W6West.php" class="btn btn-primary">Mehr informationen</a>
+                        </div>
+                    </div>
                 </div>
             </li>
 
 
             <li>
-                <div>
-                    <h1><a href="Beitraege/Bib_Bruecke_Ulhormsweg/Bib_Bruecke_Ulhormswg.php" title="Bib Brücke Ulhormsweg">
+                <div style="border: black 1px solid">
+                    <h1><a href="Beitraege/Bib_Bruecke_Ulhormsweg/Bib_Bruecke_Ulhormswg.php"  title="Bib Brücke Ulhormsweg">
                             Bib Brücke Ulhormsweg
                         </a></h1>
-                    <img src="Beitraege/Bib_Bruecke_Ulhormsweg/ink(1).png" alt="FP2" height='600'>
+                    <img src="Beitraege/Bib_Bruecke_Ulhormsweg/ink(1).png" alt="FP2" height='300'>
                     <p>-Haarentor</p>
                     <p>-Überdacht</p>
                     <p>-unter der Brücke</p>
-                    <img src="Beitraege/Bib_Bruecke_Ulhormsweg/Anmerkung%202019-04-23%20155113.png" alt="Sterne" height='600'>
+                    <img src="Beitraege/Bib_Bruecke_Ulhormsweg/Anmerkung%202019-04-23%20155113.png" alt="Sterne" height='300'>
                 </div>
             </li>
 
 
             <li>
-                <div>
+                <div style="border: black 1px solid">
                     <h1><a href="Beitraege/A2_Bruecke_Ulhormsweg/A2_Bruecke_Ulhormsweg.php" title="A2 Brücke Ulhormsweg">
                             A2 Brücke Ulhormsweg
                         </a></h1>
-                    <img src="Beitraege/A2_Bruecke_Ulhormsweg/ink%20(1).png" alt="Bild des Stellplatzes" height='600'>
+                    <img src="Beitraege/A2_Bruecke_Ulhormsweg/ink%20(1).png" alt="Bild des Stellplatzes" height='300'>
                     <p>Haarentor</p>
                     <p>Überdacht</p>
                     <p>Öffentlich</p>
-                    <img src="Beitraege/A2_Bruecke_Ulhormsweg/Anmerkung%202019-04-23%20154043.png" alt="Position des Stellplatzes" height='600'>
+                    <img src="Beitraege/A2_Bruecke_Ulhormsweg/Anmerkung%202019-04-23%20154043.png" alt="Position des Stellplatzes" height='300'>
                 </div>
             </li>
 
