f341294c9f3b28ff2025f25adb7c728e1e3844a6
Tim Behrens <timy.behrens@ewetel.net>
2019-05-09 15:45:56 +0200

    schönheits anpassungen

--- map.php
+++ map.php
@@ -22,11 +22,12 @@
     <section>
         <div id="mainFrame">
             <div class="card">
-            <h1><a Standort:</h1>
-
-
+                <h1><a :class="card-title" title="Standort">
+                        Standort
+                    </a></h1>
                 <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=7.756347656250001%2C53.0193631492509%2C8.463592529296877%2C53.23772373999386&amp;layer=mapnik" style="border: 1px solid black"></iframe><br /><small><a href="https://www.openstreetmap.org/#map=11/53.1287/8.1100">Größere Karte anzeigen</a></small>
-                     </div>
+
+            </div>
         </div>
     </section>
 
