410804b508e3662f6dffabad3eb7bfaf174f0fb5
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-16 10:38:09 +0200

    Option für die Größe und besseres Layout für die Suche

--- php/search.php
+++ php/search.php
@@ -2,7 +2,7 @@
           <div class="form-group">
               <label for="search_q">
                   Standortname
-              </label>
+              </label><br>
               <input type="text" id="search_q" name="q" value="" placeholder="Standortname" autocomplete="off" />
           </div>
 
@@ -19,6 +19,16 @@
               </label>
               <input type="checkbox" id="publicCheck" name="Öffentlich" value="public" checked>
           </div>
+
+          <div class="form-group">
+              <label >
+                  Größe
+              </label><br>
+              <input type="radio" name="size" value="male" checked> Klein<br>
+              <input type="radio" name="size" value="female"> Mittel<br>
+              <input type="radio" name="size" value="other"> Groß
+          </div>
+
 
           <button type="submit" class="btn btn-default">Suchen</button>
       </form>
