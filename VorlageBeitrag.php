af1bd5a5f38861af7b559cc7fab4526aa8746859
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-18 17:10:54 +0200

    Dateien verschoben und ein paar Pfade angepasst

--- VorlageBeitrag.php
+++ VorlageBeitrag.php
@@ -5,23 +5,23 @@
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/structure.css">
+    <link rel="stylesheet" href="css/structure.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fahrrad Stellpätze</title>
-    <link href="../Bilder/IconTransparent.png" rel="icon">
+    <link href="Bilder/IconTransparent.png" rel="icon">
 </head>
 
 <body>
-    <?php include "../php/header.php"; ?>
-    <?php include "../php/navigation.php"; ?>
+    <?php include "php/header.php"; ?>
+    <?php include "php/navigation.php"; ?>
     <div id="mainFrame">
 
         <section>
             <div class="row">
                 <div class="col col-auto">
                     <div id="sideSearch">
-                        <?php include "../php/search.php"; ?>
+                        <?php include "php/search.php"; ?>
                     </div>
                 </div>
                 <div class="col">
@@ -33,10 +33,10 @@
                         </div>
                         <div class="row">
                             <div class="col border">
-                                <img src="DummyBild.png" alt="Bild des Stellplatzes" class="img-fluid">
+                                <img src="Bilder/DummyBild.png" alt="Bild des Stellplatzes" class="img-fluid">
                             </div>
                             <div class="col border">
-                                <img src="../Bilder/DummyMaps.png" alt="Position des Stellplatzes" class="img-fluid">
+                                <img src="Bilder/DummyMaps.png" alt="Position des Stellplatzes" class="img-fluid">
                             </div>
                         </div>
                         <div class="row border">
@@ -95,13 +95,13 @@
             <h1>Kommentare</h1>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">
-                    <?php include "../php/comment.php"; ?>
+                    <?php include "php/comment.php"; ?>
                 </li>
                 <li class="list-group-item">
-                    <?php include "../php/comment.php"; ?>
+                    <?php include "php/comment.php"; ?>
                 </li>
                 <li class="list-group-item">
-                    <?php include "../php/comment.php"; ?>
+                    <?php include "php/comment.php"; ?>
                 </li>
                 <li class="list-group-item">
                     <div class="card">
