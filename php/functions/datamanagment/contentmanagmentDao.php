308ef3d2459f43fc7f80d4585123ba38725ef281
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-21 16:44:04 +0200

    Eigene Funktion für Code Abschnitt hinzugefügt

--- php/functions/datamanagment/contentmanagmentDao.php
+++ php/functions/datamanagment/contentmanagmentDao.php
@@ -57,5 +57,43 @@ function searchResult($isSearch,$name=null,$isPublic=null,$size=null,$hasRoof=nu
     }
 }
 
+function search(){
+    $isSearch =false;
+    $name = null;
+    $isPublic=null;
+    $size=0;
+    $hasRoof=null;
+    $holderType=null;
+    if(isset($_GET["SubmitSearch"])){
+        $isSearch =true;
+        if(isset($_GET["locationName"])){
+            $name = $_GET["locationName"];
+        }
+        if(isset($_GET["Öffentlich"])){
+            $isPublic = true;
+        }
+        if(isset($_GET["kleinerStellplatz"])){//klein
+            $size = $size + 1;
+        }
+        if(isset($_GET["mittlererStellplatz"])){//mittel
+            $size = $size + 2;
+        }
+        if(isset($_GET["großerStellplatz"])){//groß
+            $size = $size + 4;
+        }
+
+        if(isset($_GET["Überdacht"])){
+            $name = $_GET["locationName"];
+        }
+        if(isset($_GET["Halterungsart"])){
+            $name = $_GET["locationName"];
+        }
+    }
+
+    foreach(searchResult($isSearch,$name,$isPublic,$size,$hasRoof,$holderType) as $id) {
+        yield $id;
+    }
+}
+
 
 ?>
