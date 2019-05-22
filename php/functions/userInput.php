77679f52a5626d1dd556df5064bbdd44bd14272d
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-22 13:55:55 +0200

    Dateien werden Hochgeladen(zum Testen)

--- php/functions/userInput.php
+++ php/functions/userInput.php
@@ -44,11 +44,12 @@ function search(){
 function comment(){
     if (isset($_FILES["commentImg"])) {
         $image = $_FILES["commentImg"];
-        if (move_uploaded_file($_FILES["commentImg"]["tmp_name"],"C:\\Users\\hauke\\Desktop".$image['name'])) {
+        if (move_uploaded_file($_FILES["commentImg"]["tmp_name"],"Bilder/Kommentare/".$image['name'])) {
             echo "Da wama Bild".$image['name'];
         }
 
     }
+
 }
 
 
