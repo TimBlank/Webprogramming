3a80c630dcde13061e90f38852a73c1604902e5d
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-29 10:56:47 +0200

    userm..Impl Datei

---
+++ php/functions/datamanagment/usermanagmentImpl.php
@@ -0,0 +1,9 @@
+<?php
+
+//prüfen ob Logindaten korrekt sind
+function verifyLogin($name, $password) {}
+
+//neuenBenutzer regristrieren
+function registerUser($realName,$email,$password,$userName){}
+
+?>
