90e06fb2fe59a48fcbd055c86e0f3647626ab907
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-05-30 19:39:28 +0200

    ungetestete Version der Passwort Verifikation fertig

--- php/functions/datamanagment/usermanagmentImpl.php
+++ php/functions/datamanagment/usermanagmentImpl.php
@@ -1,7 +1,24 @@
 <?php
+include "databaseConnection.php";
 
 //prüfen ob Logindaten korrekt sind
 function verifyLogin($name, $password) {
+    try{
+        $db = databaseConnect();
+        $sql = "SELECT password FROM user WHERE username = (:loadName)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":loadName", $name);
+        $stmt->execute();
+        $hashData = $stmt->fetchObject();
+        $hash = $hashData->password
+        $db.close();
+        $db = null;
+
+    yield password_verify($password, $hash);
+
+    }catch (PDOException $ex) {
+        echo "Fehler: " . $ex->getMessage();
+   }
 }
 
 //neuenBenutzer regristrieren
