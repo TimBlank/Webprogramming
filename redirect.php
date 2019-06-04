632b764f41e2ad08e07ef813ef75a7b9757853e1
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-06-04 17:53:44 +0200

    redirect.php vervollständigt

--- redirect.php
+++ redirect.php
@@ -1,10 +1,13 @@
 <?php
+include "php/functions/datamanagment/usermanagmentDao.php";
+
 include "php/functions/datamanagment/contentmanagmentDao.php";
 
 include "php/functions/redirectFunctions/createEntryFunction";
 
+include "php/functions/redirectFunctions/createEntryFunction";
 
+include "php/functions/redirectFunctions/createEntryFunction";
 
-
-
+header("Location:http://localhost/Index.php");
 ?>
