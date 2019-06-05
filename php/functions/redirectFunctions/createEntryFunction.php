a019d2347096344a2a69a942ca6c27d8960fcb83
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-06-05 19:00:18 +0200

    createEntry korrigiert

--- php/functions/redirectFunctions/createEntryFunction.php
+++ php/functions/redirectFunctions/createEntryFunction.php
@@ -13,9 +13,12 @@ if(isset($_POST["submitEntry"])){
     $imgType =null;
     $longitude=null;
     $latitude=null;
+    $inputsCorrect = true;
 
     if(isset($_POST["entryName"])&&is_string($_POST["entryName"])) {
         $entryName = htmlspecialchars($_POST["entryName"]);
+    }else{
+        $inputsCorrect = false;
     }
 
     if(isset($_FILES["userImage"])) {
@@ -23,45 +26,61 @@ if(isset($_POST["submitEntry"])){
         $image = $_FILES["userImage"];
         if(empty($image)){
             //echo "Es wurde keine Datei Hochgeladen <br>" ;
-            return false;
-        }
+            $inputsCorrect = false;
+        }else{
             //Dateiendung
-        $imgType = strtolower(pathinfo($image["name"],PATHINFO_EXTENSION));
-        $check = getimagesize($_FILES["userImage"]["tmp_name"]);
-        if($check !==false){
-            $userImage = $image;
-        } else {
-            //echo "Das war kein Bild <br>" ;
-            return false;
+            $imgType = strtolower(pathinfo($image["name"],PATHINFO_EXTENSION));
+            $check = getimagesize($_FILES["userImage"]["tmp_name"]);
+            if($check !==false){
+                $userImage = $image;
+            } else {
+                //echo "Das war kein Bild <br>" ;
+                $inputsCorrect = false;
+            }
         }
     }
 
     if(isset($_POST["isPublic"])&&($_POST["isPublic"]=="false"||$_POST["isPublic"]=="true")) {
         $isPublic = ($_POST["isPublic"]=="true");
+    }else{
+        $inputsCorrect = false;
     }
 
     if(isset($_POST["size"])&&is_string($_POST["size"])) {
         $size = htmlspecialchars($_POST["size"]);
+    }else{
+        $inputsCorrect = false;
     }
     if(isset($_POST["hasRoof"])&&($_POST["hasRoof"]=="false"||$_POST["hasRoof"]=="true")) {
         $hasRoof = ($_POST["hasRoof"]=="true");
+    }else{
+        $inputsCorrect = false;
     }
     if(isset($_POST["holdingType"])&&is_string($_POST["holdingType"])) {
         $holdingType = htmlspecialchars($_POST["holdingType"]);
+    }else{
+        $inputsCorrect = false;
     }
     if(isset($_POST["description"])&&is_string($_POST["description"])) {
          $description = htmlspecialchars($_POST["description"]);
+    }else{
+        $inputsCorrect = false;
     }
 
     if(isset($_POST["longitude"])&&is_string($_POST["longitude"]) ){
         $longitude = (float) $_POST["longitude"];
+    }else{
+        $inputsCorrect = false;
     }
 
     if(isset($_POST["latitude"])&&is_string($_POST["latitude"]) ){
         $latitude = (float) $_POST["latitude"];
+    }else{
+        $inputsCorrect = false;
     }
 
     //TODO: Überprüfen, ob alles richtig ausgefüllt ist.
+    if($inputsCorrect){
     $id = addEntry($entryName, $isPublic, $size, $hasRoof, $holdingType, $description,$longitude,
     $latitude, $imgType);
     if($id !== false) {
@@ -78,6 +97,9 @@ if(isset($_POST["submitEntry"])){
     } else {
         //echo "Test fehlgeschlagen";
         //TODO: Fehler anzeigen.
+         header("Location:http://localhost/createEntryPage.php");
+    }
+    }else {
          header("Location:http://localhost/createEntryPage.php");
     }
     }else{
