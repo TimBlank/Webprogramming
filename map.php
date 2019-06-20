6581f81f5c399cad133a493d54dbb71adb3f4007
Yannis Odion <yannis.odion@uni-oldenburg.de>
2019-06-20 12:28:43 +0200

    Nutung der Seite nun fast vollständing möglich, wenn JavaScript deaktiviert.

--- map.php
+++ map.php
@@ -27,9 +27,21 @@
                                 </a></h1>
                             <!-- TODO Höhe adaptiv-->
                             <div class="mapBox">
-                                <div class='box'>
+                                <div class='box' id="importedMap">
                                     <iframe class="content" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=7.756347656250001%2C53.0193631492509%2C8.463592529296877%2C53.23772373999386&amp;layer=mapnik" style="border: 1px solid black"></iframe><br /><small><a href="https://www.openstreetmap.org/#map=11/53.1287/8.1100">Größere Karte anzeigen</a></small>
                                 </div>
+                                <!--Start Quelle: https://stackoverflow.com/questions/1577598/how-to-hide-parts-of-html-when-javascript-is-disabled-->
+                                <noscript>
+                                    <style>
+                                        #importedMap {
+                                            display: none;
+                                        }
+                                    </style>
+                                    <div>
+                                        <label>Zum Nutzen der Karte bitte JavaScript Aktivieren</label>
+                                    </div>
+                                </noscript>
+                                <!--Ende-->
                             </div>
                         </div>
                         <?php
