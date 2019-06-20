579dcd451f7d452f7e5b69f8d6b87a000c23a59b
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-06-20 13:33:20 +0200

    Ulr-Abfrage aus noscript LoginLogout entfernt

--- php/htmlElements/noscriptLogInOut.php
+++ php/htmlElements/noscriptLogInOut.php
@@ -1,14 +1,6 @@
-<?php
-             //Quelle: https://www.tutorialrepublic.com/faq/how-to-get-current-page-url-in-php.php
-            $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-
-                        $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        ?>
-
 <?php if(isset($_SESSION["User"])){?>
 
 <form action="redirect.php" method="post" class="form-inline dropdown-item">
-    <?php echo '<input type="hidden" name="PrevPage" value="'.$url.'" required>';?>
     <button type="submit" class="btn btn-default" name="logoutSubmit" value="logoutSubmit">
         Ausloggen
     </button>
@@ -17,7 +9,6 @@
 
 <div class="form-inline">
     <form action="redirect.php" method="post" class="form-inline dropdown-item">
-        <?php echo '<input type="hidden" name="PrevPage" value="'.$url.'" required>';?>
         <div>
             <input type="text" class="form-control" id="username" name="un" placeholder="Benutzername" autocomplete="off" required>
             <input type="password" class="form-control" id="password" name="pw" placeholder="Passwort" autocomplete="off" required>
