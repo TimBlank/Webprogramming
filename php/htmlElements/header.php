52b7b40bf28c6e45abe8ae316538b18e55485213
Yannis Odion <yannis.odion@uni-oldenburg.de>
2019-06-20 11:13:44 +0200

    Dateien in neuen Ordner verschoben.

--- php/htmlElements/header.php
+++ php/htmlElements/header.php
@@ -1,5 +1,5 @@
 <header class="bg-light">
     <h1>Fahrrad Stellplätze</h1>
     <h2>Universität Oldenburg</h2>
-    <?php include_once "php/modals.php"; ?>
+    <?php include_once "php/htmlElements/modals.php"; ?>
 </header>
