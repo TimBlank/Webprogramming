b6c88385e300ff5ee139bddf103893efcc89b81c
Tim <timy.behrens@ewetel.net>
2019-06-24 20:58:22 +0200

    Googel Satci Map

--- map.php
+++ map.php
@@ -30,6 +30,7 @@
                                 <div class='box'>
 
                                     <iframe class="content" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/d/embed?mid=1PEH14t02jnaQdPOHBk3g4pOd2qnDYgeT" style="border: 1px solid black"></iframe>
+
                                 </div>
                             </div>
                         </div>
