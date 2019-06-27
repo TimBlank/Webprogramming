3329695e1019274cfc58331bb77a1a5df6ac5429
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-06-27 14:52:24 +0200

    Texte in seperaten Dateien

--- php/htmlElements/footer.php
+++ php/htmlElements/footer.php
@@ -2,10 +2,10 @@
     <!--Besseren Weg finden, den Pfad anzugeben. Vermutlich kein Problem mehr, wenn die Seiten alle auf der selben Ebene sind.-->
     <nav class="navbar fixed-bottom navbar-light bg-dark " style="padding:0px;">
 
-        <a class="navbar-item text-light" href="../../impressum.php" title="Impressum" style="margin-left: auto;margin-right: auto;">
+        <a class="navbar-item text-light" href="impressumPrivacy.php?Impressum" title="Impressum" style="margin-left: auto;margin-right: auto;">
             Impressum
         </a>
-        <a class="navbar-item text-light" href="../../impressum.php" title="Datenschutz" style="margin-left: auto;margin-right: auto;">
+        <a class="navbar-item text-light" href="impressumPrivacy.php?Datenschutzerklärung" title="Datenschutz" style="margin-left: auto;margin-right: auto;">
             Datenschutz
         </a>
 
