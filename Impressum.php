d1cf2f4ef7ee9121d4ee3073855d07c476ee6f1b
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-06-27 13:50:49 +0200

    Sonderzeichen ersetzt

--- Impressum.php
+++ Impressum.php
@@ -18,58 +18,21 @@
                 <div class="card">
                     <!-- Impressum wurde mit der Seite https://www.e-recht24.de/impressum-generator.html erzeugt-->
                     <h1>Impressum</h1>
-                    <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
-                    <p>Ultra Unternehmensgruppe<br /> Susi-Spectum-Stra&szlig;e 42<br /> 43210 Humbug</p>
+                    <h2>Angaben gemäß § 5 TMG</h2>
+                    <p>Ultra Unternehmensgruppe<br /> Susi-Spectum-Straße 42<br /> 43210 Humbug</p>
                     <p><strong>Vertreten durch:</strong><br /> Henry Ultra</p>
                     <p>Diese vertreten durch:<br /> Olga Ultra</p>
                     <p>Handelsregister: UUG 424242<br /> Registergericht: Registergericht Humbug</p>
                     <h2>Kontakt</h2>
                     <p>Telefon: 0711-508855242<br /> E-Mail: contact@uung.com</p>
                     <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-                    <h3>Haftung f&uuml;r Inhalte</h3>
-                    <p>Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu &uuml;berwachen oder nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.</p>
-                    <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung m&ouml;glich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
-                    <h3>Haftung f&uuml;r Links</h3>
-                    <p>Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen. F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche
+                    <h3>Haftung für Inhalte</h3>
+                    <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
+                    <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+                    <h3>Haftung für Links</h3>
+                    <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche
 
                 </div>
-                <div class="card">
-                    <div class="card-body">
-                        <h2 class="card-title">Gruppe</h2>
-
-                        <ul class="list-group list-group-flush">
-                            <img id="groupImage" src="pictures/Gruppenbild.png" alt="Gruppenbild platz">
-                            <li class="list-group-item">Von Links nach rehts sind die Entwickler dieser Seite: </li>
-                            <li class="list-group-item">Yannis Odion: yannis.odion@uol.de</li>
-                            <li class="list-group-item">Hauke Tammen: hauke.tammen@uol.de</li>
-                            <li class="list-group-item">Tim Behrens: tim.behrens@uol.de</li>
-                        </ul>
-                    </div>
-                </div>
-
-
-                <div class="card">
-                    <div class="card-body">
-                        <h2 class="card-title">Wichtige Date</h2>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Uni Oldenbrug</li>
-                            <li class="list-group-item">Webprogrammierung: inf016</li>
-                            <li class="list-group-item">Professor: Dr.-Ing. Dietrich Boles</li>
-                            <li class="list-group-item">Email: dietrich.boles@uni-oldenburg.de</li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div class="card-body">
-                        <h2 class="card-title">Eigenerschaffungs Erklärung</h2>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Hiermit erklären wir, dass diese Seite von uns ihm unviersitären Rahmen erstellt wurde und von der Universität: Universität Oldenburg genutzt und weiter bearbeitet werden darf.</li>
-
-                        </ul>
-                    </div>
-                </div>
-
             </section>
         </div>
     </div>
