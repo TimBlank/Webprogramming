3919239544a9a9311b39cc2f6f0a3b21b0836933
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-06-27 13:46:00 +0200

    Impressums-Text hinzugfügt

--- Impressum.php
+++ Impressum.php
@@ -16,6 +16,24 @@
         <div id="mainFrame">
             <section>
                 <div class="card">
+                    <!-- Impressum wurde mit der Seite https://www.e-recht24.de/impressum-generator.html erzeugt-->
+                    <h1>Impressum</h1>
+                    <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+                    <p>Ultra Unternehmensgruppe<br /> Susi-Spectum-Stra&szlig;e 42<br /> 43210 Humbug</p>
+                    <p><strong>Vertreten durch:</strong><br /> Henry Ultra</p>
+                    <p>Diese vertreten durch:<br /> Olga Ultra</p>
+                    <p>Handelsregister: UUG 424242<br /> Registergericht: Registergericht Humbug</p>
+                    <h2>Kontakt</h2>
+                    <p>Telefon: 0711-508855242<br /> E-Mail: contact@uung.com</p>
+                    <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+                    <h3>Haftung f&uuml;r Inhalte</h3>
+                    <p>Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu &uuml;berwachen oder nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.</p>
+                    <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung m&ouml;glich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+                    <h3>Haftung f&uuml;r Links</h3>
+                    <p>Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen. F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche
+
+                </div>
+                <div class="card">
                     <div class="card-body">
                         <h2 class="card-title">Gruppe</h2>
 
--- php/htmlElements/footer.php
+++ php/htmlElements/footer.php
@@ -2,8 +2,11 @@
     <!--Besseren Weg finden, den Pfad anzugeben. Vermutlich kein Problem mehr, wenn die Seiten alle auf der selben Ebene sind.-->
     <nav class="navbar fixed-bottom navbar-light bg-dark " style="padding:0px;">
 
-        <a class="navbar-item text-light" href="../../impressum.php" title="Impressum und Datenschutz" style="margin-left: auto;margin-right: auto;">
-            Impressum und Datenschutz<br>
+        <a class="navbar-item text-light" href="../../impressum.php" title="Impressum" style="margin-left: auto;margin-right: auto;">
+            Impressum<br>
+        </a>
+        <a class="navbar-item text-light" href="../../impressum.php" title="Datenschutz" style="margin-left: auto;margin-right: auto;">
+            Datenschutz<br>
         </a>
 
     </nav>
