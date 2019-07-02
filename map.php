9176ca060866fce699b2cabfcf71dae079106b2e
Tim <timy.behrens@ewetel.net>
2019-07-02 02:31:27 +0200

    Ende Nachtschicht...nicht fertig

--- map.php
+++ map.php
@@ -4,6 +4,13 @@
 <html lang="de">
 
 <head>
+    <style>
+        #maps {
+            height: 500px
+        }
+
+    </style>
+
     <title>Map</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
@@ -33,9 +40,11 @@
                             <!-- TODO Höhe adaptiv-->
                             <div class="mapBox">
                                 <div class='box'>
-
+                                    <div id="map">
+                                    </div>
+                                    <!--
                                     <iframe class="content" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/d/embed?mid=1PEH14t02jnaQdPOHBk3g4pOd2qnDYgeT" style="border: 1px solid black"></iframe>
-
+-->
                                 </div>
                             </div>
                         </div>
@@ -50,35 +59,23 @@
         </section>
     </div>
     <div>
-        <div id="map">
-            <script>
-                var map;
 
-                function initMap() {
-                    map = new google.maps.Map(document.getElementById('map'), {
-                        zoom: 10,
-                        center: new google.maps.LatLng(42, 42),
-                        mapTypid: 'terrain'
-                    })
-                    var script = document.createElement('script');
-                }
-
-            </script>
-        </div>
     </div>
     <!--TODO    Bereiche: einteilen
                 Marker:   setzen
                 Webseite: https://wiki.openstreetmap.org/wiki/OpenLayers
     -->
+    <!--Googlemaps script -->
+    <script src="mapscript.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?callback=creatMap&key=AIzaSyA7xuOTZWXpbH7CgL8f7ZS9_pGUygPPYro" async defer></script>
+
 
     <?php include "php/footer.php"; ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-    <!--Googlemaps script -->
-    <script src="mapscript.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7xuOTZWXpbH7CgL8f7ZS9_pGUygPPYro" async defer></script>
+
 </body>
 
 </html>
--- mapscript.js
+++ mapscript.js
@@ -1,12 +1,12 @@
-var map, infoWindow;
+var map;
 
 function creatMap(){
-    var a=42
-    var b=22
-    var diff= 0.0033
+   // var a=42
+   // var b=22
+   // var diff= 0.0033
 
     var options ={
-        center:{lat: a, lng: b},
+        center:{lat: 42, lng: 22},
         zoom : 14
         //disabelDefaultUI: true    //für Karte auf Hauptseite
         //drarggable: false         // für Karte auf Hauptseite
