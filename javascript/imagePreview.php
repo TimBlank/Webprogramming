22f1f2c9bc6d8167c6bf4a6231511fe4e78d037b
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-07-03 18:01:31 +0200

    Fehler behoben

--- javascript/imagePreview.php
+++ javascript/imagePreview.php
@@ -1,40 +1,40 @@
 <script>
     function isInAspectRatio(nA, nB) {
-    if(nB > nA) {
-        // "Bilder im Hochformat werden nicht unterstützt.";
-        return false;
-    }else if(nA < nB * 2.5) {
-        return true;
-    }else{
-        // "Das Bild ist in einem zu breiten Seitenverhältnis.";
-        return false;
+        if (nB > nA) {
+            // "Bilder im Hochformat werden nicht unterstützt.";
+            return false;
+        } else if (nA < nB * 2.5) {
+            return true;
+        } else {
+            // "Das Bild ist in einem zu breiten Seitenverhältnis.";
+            return false;
+        }
     }
-}
 
-
-
-
-
-
-
-
-    //Quelle: https://stackoverflow.com/questions/14791247/how-to-create-image-uploader-with-preview
+    //Quellen:
+    //-https://stackoverflow.com/questions/14791247/how-to-create-image-uploader-with-preview
+    //-https://stackoverflow.com/questions/15491193/getting-width-height-of-an-image-with-filereader
     function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
             reader.onload = function(e) {
                 $('#imagePreview').attr('src', e.target.result);
+
                 var image = new Image();
+
                 image.src = e.target.result;
-                var width = image.width;
-                var height = image.height;
-                if (isInAspectRatio(height, width)){
-                    $('#imageError').empty();
-                    $('#imageError').append("Ungültiges Seitenverhältnis");
-                }else{
-                    $('#imageError').empty();
-                    $('#imageError').append(" ");
+
+                image.onload = function() {
+                    var width = image.width;
+                    var height = image.height;
+                    if (isInAspectRatio(height, width)) {
+                        $('#imageError').empty();
+                        $('#imageError').append("Ungültiges Seitenverhältnis");
+                    } else {
+                        $('#imageError').empty();
+                        $('#imageError').append(" ");
+                    }
                 }
 
             }
