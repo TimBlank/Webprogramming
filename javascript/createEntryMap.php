7f3032b78a626963ef61fea485346f4effd44d39
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-07-09 17:53:41 +0200

    JavaScript  ausgelagert

---
+++ javascript/createEntryMap.php
@@ -0,0 +1,90 @@
+    <script>
+        var map = null;
+        var marker = null;
+
+        function changeInputCoordinates(longitude, latitude) {
+            document.getElementById("longitude").value = longitude;
+            document.getElementById("latitude").value = latitude;
+        }
+
+        function getPosition() {
+            if (navigator.geolocation) {
+                var options = {
+                    enableHighAccuracy: true
+                }
+                navigator.geolocation.getCurrentPosition(showPosition, showError, options);
+            } else {
+                alert('Browser unterstützt kein Geolocation.');
+            }
+        }
+
+        function showPosition(position) {
+            var longitude = position.coords.longitude;
+            var latitude = position.coords.latitude;
+            changeInputCoordinates(longitude, latitude);
+            var latlng = new google.maps.LatLng(latitude, longitude);
+            if (map !== null) {
+                map.panTo(latlng);
+                if (marker == null) {
+                    addMarker(latlng, map);
+                }
+                marker.setPosition(latlng);
+            }
+        }
+
+        function showError(error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    alert('Abfrage ihrer Geoposition ist untersagt.');
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    alert('Keine Geopositionsdaten verfügbar.');
+                    break;
+                case error.TIMEOUT:
+                    alert('Timeout überschritten.');
+                    break;
+                default:
+                    alert('Fehler: ' + error.message + ')');
+            }
+
+        }
+
+        var markerSet = 0;
+
+        function initMap() {
+            var location = {
+                lat: 53.147294,
+                lng: 8.180886
+            };
+            map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 10,
+                center: location
+            });
+            // This event listener calls addMarker() when the map is clicked.
+            google.maps.event.addListener(map, 'click', function(event) {
+                addMarker(event.latLng, map);
+            });
+
+        }
+
+        // Adds a marker to the map.
+        function addMarker(location, map) {
+            // Add the marker at the clicked location, and add the next-available label
+            // from the array of alphabetical characters.
+            if (markerSet == 0) {
+                markerSet = 1;
+                marker = new google.maps.Marker({
+                    position: location,
+                    map: map,
+                    draggable: true
+                });
+                changeInputCoordinates(location.lng(), location.lat());
+                google.maps.event.addListener(marker, 'dragend', function() {
+                    changeInputCoordinates(marker.getPosition().lng(), marker.getPosition().lat());
+                });
+            };
+        }
+
+    </script>
+    <script async defer src=" https://maps.googleapis.com/maps/api/js?key=AIzaSyDG6fPCUYbyDko0vrNu4vZvR_Yz5jVNvik&callback=initMap ">
+    </script>
