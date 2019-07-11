64099b75498772988520ecb9712b5c767a1538db
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-07-11 12:44:13 +0200

    Kartenseite bis auf korrekte Zoom und Mittelpunkt-Werte fertig

--- map.php
+++ map.php
@@ -15,7 +15,6 @@
     <?php include_once "php/htmlElements/header.php"; ?>
     <?php include_once "php/htmlElements/navigation.php"; ?>
 
-
     <div id="background">
         <section>
             <div id="mainFrame">
@@ -26,6 +25,47 @@
                             <?php include "php/htmlElements/search.php"; ?>
                         </div>
                     </div>
+                    <?php
+                                $campusSite = "default";
+                                if(isset($_GET["campusSite"])){
+                                    $campusSite = htmlspecialchars($_GET["campusSite"]);
+                                }
+                                if($campusSite==="Haarentor"){
+                                    echo "<p hidden class='option zoom'>17</p>";
+                                    echo "<p hidden class='option longitude'>8,181954603928261</p>";
+                                    echo "<p hidden class='option latitude'>53.147294</p>";
+                                }elseif($campusSite==="Johann-Justus-Weg"){
+                                    echo "<p hidden class='option zoom'>16</p>";
+                                    echo "<p hidden class='option longitude'>8.180886</p>";
+                                    echo "<p hidden class='option latitude'>53,146797826519055</p>";
+                                }elseif($campusSite==="Botanischer Garten"){
+                                    echo "<p hidden class='option zoom'>16</p>";
+                                    echo "<p hidden class='option longitude'>8.180886</p>";
+                                    echo "<p hidden class='option latitude'>53.147294</p>";
+                                }elseif($campusSite==="Wechloy"){
+                                    echo "<p hidden class='option zoom'>16</p>";
+                                    echo "<p hidden class='option longitude'>8.180886</p>";
+                                    echo "<p hidden class='option latitude'>53.147294</p>";
+                                }elseif($campusSite==="Offis"){
+                                    echo "<p hidden class='option zoom'>16</p>";
+                                    echo "<p hidden class='option longitude'>8.180886</p>";
+                                    echo "<p hidden class='option latitude'>53.147294</p>";
+                                }else{
+                                    echo "<p hidden class='option zoom'>13</p>";
+                                    echo "<p hidden class='option longitude'>8.180886</p>";
+                                    echo "<p hidden class='option latitude'>53.147294</p>";
+                                }
+                        ?>
+                    <ul hidden>
+                        <?php foreach($contentmanager->defaultEntries() as $id):?>
+                        <?php $entry = $contentmanager->loadEntry($id);?>
+                        <li id="<?php echo $entry->getId();?>" class="entry">
+                            <p class="entryName"><?php echo $entry->getName();?></p>
+                            <p class="longitude"><?php echo $entry->getLongitude();?></p>
+                            <p class="latitude"><?php echo $entry->getLatitude();?></p>
+                        </li>
+                        <?php endforeach; ?>
+                    </ul>
                     <div class="col">
                         <div class="card">
                             <h1><a :class="card-title" title="Standort:">
@@ -52,52 +92,54 @@
                         <script>
                             function initMap() {
                                 var location = {
-                                    lat: 53.147294,
-                                    lng: 8.180886
+                                    lat: parseFloat($(".option.latitude").text().replace(",", ".")),
+                                    lng: parseFloat($(".option.longitude").text().replace(",", "."))
                                 };
 
 
                                 var map = new google.maps.Map(document.getElementById("map"), {
-                                    zoom: 10,
+                                    zoom: parseInt($(".option.zoom").text()),
                                     center: location,
 
                                 });
                                 var label = "uni";
-                                // Add a marker at the center of the map.
-                                addMarker(location, map, label);
-                                /*  for (i=$entry.length,i>0,i--){
-                                      if ("<?php //echo $content->getImage(); ?>"== "name des Ortes"){
-                                      var markLocation(){
-                                      lat: <?php //echo $content->getLatitude(); ?> ,
-                                      lng: <?php //echo $content->getLongitude(); ?>
-                                      }
-                                      addMarker(markLocation, map,label);
-                                      }
-                                  }*/
+                                // Add a markers for every entry.
+                                $('.entry').each(function() {
+                                    var entryId = this.id;
+                                    var entryName = $(".entryName", this).text();
+                                    var longitude = parseFloat($(".longitude", this).text().replace(",", "."));
+                                    var latitude = parseFloat($(".latitude", this).text().replace(",", "."));
+                                    var latlng = new google.maps.LatLng(latitude, longitude);
+                                    addMarker(latlng, map, entryId, entryName);
+                                });
+
                             }
 
                             // Adds a marker to the map.
-                            function addMarker(location, map, label) {
-                                // Add the marker at the clicked location, and add the next-available label
-                                // from the array of alphabetical characters.
+                            function addMarker(location, map, entryId, entryName) {
                                 var marker = new google.maps.Marker({
                                     position: location,
-                                    label: label,
+                                    label: entryName,
+                                    //label: content.getName(),
                                     map: map
                                 });
 
+
+                                marker.addListener('click', function() {
+                                    window.location = "entryPage.php?EntryID=" + entryId;
+                                });
                             }
 
                         </script>
                         <script async defer src=" https://maps.googleapis.com/maps/api/js?key=AIzaSyDG6fPCUYbyDko0vrNu4vZvR_Yz5jVNvik&callback=initMap "></script>
 
-
-                    </div>
-                    <?php
+                        <?php
                         if(isset($_SESSION["User"])){
                         echo "<a href=\"createEntryPage.php\" class=\"btn btn-primary\" title=\"VorlageBeitrag\">Neuen Stellplatz hinzufügen</a>";
                         }
                         ?>
+                    </div>
+
                 </div>
             </div>
         </section>
