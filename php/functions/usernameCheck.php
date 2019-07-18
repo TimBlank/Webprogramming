7370387c8e618e92798eb1682ca3df26b9e481bd
Tagehar <hauke.tammen@uni-oldenburg.de>
2019-07-18 12:56:45 +0200

    Erste Datei hinzugefügt

---
+++ php/functions/usernameCheck.php
@@ -0,0 +1,28 @@
+
+<?php
+//https://www.w3schools.com/xml/ajax_php.asp
+include_once
+
+// get the q parameter from URL
+$q = $_REQUEST["name"];
+
+
+
+// lookup all hints from array if $q is different from ""
+if ($q !== "") {
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+
+// Output "no suggestion" if no hint was found or output correct values
+echo $hint === "" ? "no suggestion" : $hint;
+?>
